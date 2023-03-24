9b2003441389a84d41d16818d62869c5d746ffdd
Chuyto70 <jjhurtado016@gmail.com>
2023-03-24 19:38:57 -0400

    cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = require("./routes/routes");
 const app = express();
+const cors = require('cors')
 const fileUpload = require("express-fileupload");
+app.use(cors())
 app.use(fileUpload());
 app.use(router);
 
